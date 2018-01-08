dfe3d1dcf2ef271b21696faef06cbb34286e68b1
Javad Shafique <javadshafique@hotmail.com>
2018-01-08 11:29:21 +0100

    Added git url

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 setup(
     name="argc",
-    version="0.2.1",
+    version="0.2.2",
     description="A argument parsing module for python 2 and 3",
     author="Javad Shafique",
     author_email="javadshafique@hotmail.com",
     license="MIT",
     packages=["argc"],
     long_description=open("README.md", "r").read(),
-    include_package_data=True
+    include_package_data=True,
+    url="https://github.com/JavadSM/argc"
 )
