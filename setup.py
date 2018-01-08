f5df5f1acec7025143d6eaaca11f2b0ab59c0082
Javad Shafique <javadshafique@hotmail.com>
2018-01-08 12:36:04 +0100

    support for markdown

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name="argc",
-    version="1.0.0",
+    version="1.0.1",
     description="A argument parsing module for python 2 and 3",
     author="Javad Shafique",
     author_email="javadshafique@hotmail.com",
     license="MIT",
     packages=["argc"],
-    long_description=open("README.md", "r").read(),
     include_package_data=True,
-    url="https://github.com/JavadSM/argc"
+    url="https://github.com/JavadSM/argc",
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md'
 )
