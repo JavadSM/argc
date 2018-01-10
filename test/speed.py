7db065c357720a6a97a4a6313d9c4effcdefced1
Javad Shafique <javadshafique@hotmail.com>
2018-01-10 10:43:04 +0100

    Added some more tests

--- test/speed.py
+++ test/speed.py
@@ -7,9 +7,15 @@ args = argc.argc(arguments, False)
 
 args.set("-x", "--extra", "x", "an x-option", 0)
 args.set("-n", "--num", "num", "Sets number", 10)
+timeit.default_number
+gd1 = timeit.timeit("args.generate_docs(True)", setup="from __main__ import args")
+gd0 = timeit.timeit("args.generate_docs(False)", setup="from __main__ import args")
 
-gd1 = timeit.timeit("args.generate_docs(True)", setup="from __main__ import args", number=100)
-gd0 = timeit.timeit("args.generate_docs(False)", setup="from __main__ import args", number=100)
+load1 = timeit.timeit("a = argc.argc(arguments, False)", setup="from __main__ import argc, arguments")
+load2 = timeit.timeit("a = argc.argc(arguments, True)", setup="from __main__ import argc, arguments")
 
-print(gd0)
-print(gd1)
+print("Generate Docs, with compact:", gd0)
+print("Generate Docs, without compact:", gd1)
+
+print("Load with detect", load2)
+print("Load without detect", load1)
